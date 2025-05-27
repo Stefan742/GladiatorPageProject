bc8c9b70b35ad5416b519ba8e8c9b555c3782ee1
Stefan Stojanovski <stefanstojanovski21@gmail.com>
2025-05-28 00:05:46 +0200

    added deploy files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,8 +6,6 @@
     <AboutUs />
     <TrainingSchedule />
     <GallerySection />
-
-
     <button v-if="showScrollButton" @click="scrollToTop" class="scroll-to-top">
       <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="none" viewBox="0 0 24 24" stroke="black" stroke-width="2">
         <path stroke-linecap="round" stroke-linejoin="round" d="M5 15l7-7 7 7" />
